0bb2295a3c4c62754fd06bd292d43f80c963bbe9
Umayah Abdennabi <abden003@gmail.com>
2014-11-21 14:24:09 -0600

    Added some comments

--- scrapModels.py
+++ scrapModels.py
@@ -1,6 +1,7 @@
 import urllib2 
 import re
 
+# List of all the possible carriers that we could potentially care about
 phoneCompanies = ['Acer', 'Alcatel', 'Allview', 'Amazon', 'Amoi', 'Apple', 'Archos', 'Asus', 'AT&T', 'Benefon', 'BenQ', 'BenQ-Siemens', 'Bird', 'BlackBerry', 'BLU', 'Bosch', 'Casio', 'Cat', 'Celkon', 'Chea', 'Dell', 'Emporia', 'Ericsson', 'Eten', 'FujitsuSiemens', 'Garmin-Asus', 'Gigabyte', 'Gionee', 'Haier', 'HP', 'HTC', 'Huawei', 'i-mate', 'i-mobile', 'Icemobile', 'Innostream', 'iNQ', 'Jolla', 'Karbonn', 'Kyocera', 'Lava', 'Lenovo', 'LG', 'Maxon', 'Maxwest', 'Meizu', 'Micromax', 'Microsoft', 'Mitac', 'Mitsubishi', 'Modu', 'Motorola', 'MWg', 'NEC', 'Neonode', 'NIU', 'Nokia', 'Nvidia', 'O2', 'OnePlus', 'Oppo', 'Orange', 'Palm', 'Panasonic', 'Pantech', 'Parla', 'Philips', 'Plum', 'Prestigio', 'Qtek', 'Sagem', 'Samsung', 'Sendo', 'Sewon', 'Sharp', 'Siemens', 'Sonim', 'Sony', 'SonyEricsson', 'Spice', 'T-Mobile', 'Tel.Me.', 'Telit', 'Thuraya', 'Toshiba', 'Unnecto', 'Vertu', 'verykool', 'Vivo', 'VKMobile', 'Vodafone', 'Wiko', 'WND', 'XCute', 'Xiaomi', 'XOLO', 'Yezz', 'ZTE']
 
 # Change all to lowercase since the html page uses lower case phone names
@@ -15,6 +16,7 @@ for i in range(1,99):
 	k = response.read()
 	# The site has php files for each model, scrap these
 	models = re.findall(r'[A-Za-z0-9_-]*.php', k);
+	# Go throught the parsed input, and take the relevant data and parse it further
 	for i in models:
 		if 'phones' not in i and 'review' not in i:
 			i = i.replace('-', ' ')
